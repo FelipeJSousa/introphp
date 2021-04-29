3bcbb8bffce807ec8ffb3089f049f3a498c2b058
theylkszaszin <csfelipe8@gmai.com>
2021-04-29 02:01:01 +0000

    Iniciado a construção da master

---
+++ layout/master.php
@@ -0,0 +1,34 @@
+<?php
+
+    function top(){
+        include_once '/var/www/html/felipe/introphp/funcoes.php';
+        echo  '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+            <title>Bem vindo ao nosso projeto!</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="/var/www/html/felipe/introphp/style/style.css">
+            <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+        </head>
+        <body>';
+
+        include_once '/var/www/html/felipe/introphp/cabecalho.php';
+        echo '<div class="page">';
+        include_once '/var/www/html/felipe/introphp/menu.php';
+
+    }
+
+    function bottom(){
+        echo  '<div class="text-center">Deseja sair desta página? <br><a href="../logout.php">Logout</a></div>
+            </div>';
+            include_once '/var/www/html/felipe/introphp/rodape.php';
+        echo   '</body></html>';
+    }
--- pages/home/index.php
+++ pages/home/index.php
@@ -1,40 +1,14 @@
 <?php
 	$page = "home";
+    include_once '../../layout/master.php';
+    top();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
-<title>Bem vindo ao nosso projeto!</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="../../style/style.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
-</head>
-<body>
-	<?php 
-		include_once '../../cabecalho.php';
-	?>
-<div class="signup-form page">
-	<?php
-		include_once '../../menu.php';
-        include_once '../../funcoes.php';
-        verificaLogin($page);
-	?>
-	
-    <form action="home.php" method="post" enctype="multipart/form-data">
-		<h2>Bem-vindo</h2>
-        <br>
-        <?php
-            print_r($Conteudos[$page]);
-        ?>
+<form action="home.php" method="post" enctype="multipart/form-data">
+    <h2>Bem-vindo</h2>
+    <br>
+<?php
+    print_r($Conteudos[$page]);
+?>
 
             
 		<p class="hint-text"><br><b>Bem-vindo </b><?php echo $_SESSION["Nome"] ?> <?php echo $_SESSION["Sobrenome"] ?></p>
